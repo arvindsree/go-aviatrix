f27af2d03bd18e6c17321ceec08af2087d3e1cf0
Arvind <arvind@aviatrix.com>
2018-11-16 19:28:00 -0800

    Test

--- goaviatrix/tags.go
+++ goaviatrix/tags.go
@@ -41,7 +41,7 @@ func (c *Client) DeleteTags(tags *Tags) (error) {
 	tags.Action="delete_resource_tags"
 	verb := "POST"
 	body := fmt.Sprintf("CID=%s&action=%s&cloud_type=%d&resource_type=%s&resource_name=%s&del_tag_list=%s", c.CID, tags.Action, tags.CloudType, tags.ResourceType ,tags.ResourceName ,tags.TagList)
-	log.Printf("[TRACE] %s %s Body: %s", verb, c.baseURL, body)
+	log.Printf("[TRACE] %s %s  Body: %s", verb, c.baseURL, body)
 	req, err := http.NewRequest(verb, c.baseURL, strings.NewReader(body))
 	if err == nil {
 		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
